cb3d3164f644ea2a2ea6facf4fa69b97b9fdd6ec
titix <titi@defi.sucks>
2024-04-29 10:02:10 -0300

    feat: thanks copy

--- src/containers/TweetCard.tsx
+++ src/containers/TweetCard.tsx
@@ -3,15 +3,27 @@ import { Tweet } from 'react-tweet';
 
 export const TweetCard = () => {
   return (
-    <Box>
-      <Typography></Typography>
+    <TweetContainer>
+      <SText>
+        Thanks to Worldcoin, Richard was able to buy his first 🐐 After that, he went viral on Twitter Now we want to
+        help him get more goats
+      </SText>
       <SBox className='dark'>
         <Tweet id='1774728635145965672' />
         <Tweet id='1775775458598306029' />
       </SBox>
-    </Box>
+    </TweetContainer>
   );
 };
+
+const TweetContainer = styled(Box)({
+  display: 'block',
+  justifyContent: 'center',
+});
+
+const SText = styled(Typography)({
+  width: '35rem',
+});
 
 const SBox = styled(Box)({
   zIndex: 100,
