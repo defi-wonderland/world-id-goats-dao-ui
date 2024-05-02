a7037dc2e817a1ffac8d20a23173e95f6589853f
titix <titi@defi.sucks>
2024-05-02 15:46:07 -0300

    fix: mobile responsive

--- src/containers/VotingCard.tsx
+++ src/containers/VotingCard.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import { Box, Typography, styled } from '@mui/material';
+import { useAccount } from 'wagmi';
 
 import { ProposalPoll, Voting, CountdownTimer, Title } from '~/components';
 import { useContract } from '~/hooks';
 import { getConfig } from '~/config';
-import { useAccount } from 'wagmi';
 
 const { PROPOSAL_ID } = getConfig();
 
@@ -47,23 +47,31 @@ export const VotingCard = () => {
   }, [address, getHasVoted, getProposalDeadline, getProposalState, txHash]);
 
   return (
-    <SBox>
-      <Title title="Should Wonderland contribute 250 WLD to Richard's goat project?" />
+    <>
+      {status != 1 && (
+        <GoatBgContainer>
+          <Goat1>🐐</Goat1>
+          <Goat2>🐐</Goat2>
+        </GoatBgContainer>
+      )}
 
-      <VotingContainer>
-        {!addressVoted && status === 1 && <Voting />}
+      <SBox>
+        <Title title="Should Wonderland contribute 250 WLD to Richard's goat project?" />
+        <VotingContainer>
+          {!addressVoted && status === 1 && <Voting />}
 
-        <ProposalPoll />
+          <ProposalPoll />
 
-        {addressVoted && status === 1 && <SecondaryText>THANKS FOR YOUR VOTE</SecondaryText>}
+          {addressVoted && status === 1 && <SecondaryText>THANKS FOR YOUR VOTE</SecondaryText>}
 
-        {deadline && status === 1 && <CountdownTimer targetDate={deadline} />}
+          {deadline && status === 1 && <CountdownTimer targetDate={deadline} />}
 
-        {status != 1 && <SecondaryText>VOTING ENDED</SecondaryText>}
+          {status != 1 && <SecondaryText>VOTING ENDED</SecondaryText>}
 
-        {status != 1 && <EndText>🐐 THANKS FOR YOUR HELPING RICHARD OUT 🐐</EndText>}
-      </VotingContainer>
-    </SBox>
+          {status != 1 && <EndText>🐐 THANKS FOR YOUR HELPING RICHARD OUT 🐐</EndText>}
+        </VotingContainer>
+      </SBox>
+    </>
   );
 };
 
@@ -78,6 +86,15 @@ export const SBox = styled(Box)(() => {
     width: '100%',
     '@media (max-width: 600px)': {
       margin: '1rem',
+    },
+    '@media (min-width: 601px) and (max-width: 1024px)': {
+      margin: '1.5rem 0 7rem 0',
+    },
+    '@media (min-width: 1025px) and (max-width: 1440px)': {
+      margin: '4rem 0 13rem 0',
+    },
+    '@media (min-width: 1441px)': {
+      margin: '6rem 0 16rem 0',
     },
   };
 });
@@ -109,4 +126,32 @@ export const EndText = styled(Typography)(() => {
     letterSpacing: '0.25rem',
     textTransform: 'uppercase',
   };
+});
+
+const GoatBgContainer = styled(Box)({
+  position: 'absolute',
+  zIndex: -1,
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100vh',
+  overflow: 'hidden',
+  display: 'flex',
+  justifyContent: 'space-around',
+});
+
+const Goat1 = styled(Typography)({
+  fontSize: '17rem',
+  position: 'absolute',
+  transform: 'scaleX(-1) rotate(35deg)',
+  top: '16rem',
+  left: '4rem',
+});
+
+const Goat2 = styled(Typography)({
+  fontSize: '17rem',
+  position: 'absolute',
+  right: '-5rem',
+  top: '2rem',
+  transform: 'rotate(45deg)',
 });
