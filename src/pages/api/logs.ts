d10a6771efa486265636759d4a0a4f6964d02f5a
titix <titi@defi.sucks>
2024-05-08 15:46:31 -0300

    fix: response

--- src/pages/api/logs.ts
+++ src/pages/api/logs.ts
@@ -3,10 +3,11 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     const { id, proof, error } = req.query;
+    const logData = { id, proof, error };
 
-    console.log({ id, proof, error });
+    console.log(logData);
 
-    res.status(200).json({ message: 'Log received successfully' });
+    res.status(200).json({ message: 'Log successful', log: logData });
   } else {
     res.setHeader('Allow', ['GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
