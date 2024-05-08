7e4e7ee838f3d0804cf4fd42e5882aaae54dbff5
titix <titi@defi.sucks>
2024-05-08 17:10:40 -0300

    fix: post req

--- src/pages/api/logs.ts
+++ src/pages/api/logs.ts
@@ -1,15 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    const { id, merkle_root, nullifier_hash, proof, error } = req.query;
+  if (req.method === 'POST') {
+    const { id, merkle_root, nullifier_hash, proof, error } = req.body;
     const logData = { id, merkle_root, nullifier_hash, proof, error };
 
     console.log(logData);
 
     res.status(200).json({ message: 'Log successful', log: logData });
   } else {
-    res.setHeader('Allow', ['GET']);
+    res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
